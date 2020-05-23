dc22657e9f9d181382a4be5af784e9d972686dcd
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-05-23 23:36:16 +0300

    new model registered

--- music/admin.py
+++ music/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, Author, InfoSource
+from .models import Post, Author, InfoSource, WordsOfWisdom
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Author)
 admin.site.register(InfoSource)
+admin.site.register(WordsOfWisdom)
